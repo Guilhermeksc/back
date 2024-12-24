9b30e26dc9fb1ac3aa7c81b87439e41c01528830
GuilhermeK <guilhermekscampos@gmail.com>
2024-12-24 14:01:45 -0300

    att

--- backend/settings.py
+++ backend/settings.py
@@ -16,7 +16,7 @@ else:
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+print(f"FRONTEND_BASE_URL is set to: {FRONTEND_BASE_URL}")
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
