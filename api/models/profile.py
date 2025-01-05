c37f8fcf2ab38a63233c2c9de7fa16c0da8b62de
GuilhermeK <guilhermekscampos@gmail.com>
2025-01-05 11:04:45 -0300

    att

--- api/models/profile.py
+++ api/models/profile.py
@@ -4,7 +4,8 @@ from django.db import models
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, related_name="profile")
     validation_token = models.CharField(max_length=64, blank=True, null=True)
-    reset_password_token = models.CharField(max_length=64, null=True, blank=True)  # Novo campo
+    reset_password_token = models.CharField(max_length=64, null=True, blank=True)
     uasg = models.CharField(max_length=20, blank=True, null=True)
+
     def __str__(self):
         return f"Perfil de {self.user.email}"
--- api/signals.py
+++ api/signals.py
@@ -1,20 +1,15 @@
 # api/signals.py
 
 from django.db.models.signals import post_save
-from django.db.models.signals import post_migrate
 from django.dispatch import receiver
 from django.contrib.auth.models import User
 from .models import Profile
-from django.db import connection
 
 @receiver(post_save, sender=User)
-def create_profile(sender, instance, created, **kwargs):
-    """
-    Cria automaticamente um perfil para novos usuários.
-    """
+def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        try:
-            Profile.objects.create(user=instance)
-            print(f"Perfil criado para o usuário: {instance.username}")
-        except Exception as e:
-            print(f"Erro ao criar perfil para o usuário {instance.username}: {e}")
+        Profile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.profile.save()
