c3596f167115cf5447980188e84e261f1aab0e5d
GuilhermeK <guilhermekscampos@gmail.com>
2025-01-05 12:20:31 -0300

    att

--- app_planejamento/signals.py
+++ app_planejamento/signals.py
@@ -6,11 +6,9 @@ from api.models import Profile
 @receiver(post_save, sender=User)
 def create_user_profile(sender, instance, created, **kwargs):
     if created:
-        # Só cria o Profile se ele ainda não existir
         Profile.objects.get_or_create(user=instance)
 
 @receiver(post_save, sender=User)
 def save_user_profile(sender, instance, **kwargs):
-    # Salva o Profile existente
     if hasattr(instance, 'profile'):
         instance.profile.save()
