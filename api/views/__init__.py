c08d42e07104187cece1aee4e699139dd76ff2c1
GuilhermeK <guilhermekscampos@gmail.com>
2025-01-06 20:47:34 -0300

    att

--- api/views/__init__.py
+++ api/views/__init__.py
@@ -1,6 +1,3 @@
-from .frontend import FrontendAppView, index
 from .auth import *
-from .password import SendPasswordResetLinkView, ChangePasswordView
-# from .email_validation import ValidateEmailView
 from .api import *
 from .licitacao import *
--- api/views/frontend.py
+++ api/views/frontend.py
@@ -1,18 +1,18 @@
-from django.shortcuts import render
-from django.views.generic import TemplateView
-from django.http import Http404
-from django.conf import settings
-import os
+# from django.shortcuts import render
+# from django.views.generic import TemplateView
+# from django.http import Http404
+# from django.conf import settings
+# import os
 
-def index(request):
-    return render(request, 'index.html') 
+# def index(request):
+#     return render(request, 'index.html') 
 
 
-class FrontendAppView(TemplateView):
-    template_name = "index.html"
+# class FrontendAppView(TemplateView):
+#     template_name = "index.html"
 
-    def get(self, request, *args, **kwargs):
-        template_path = os.path.join(settings.TEMPLATES[0]['DIRS'][0], self.template_name)
-        if not os.path.exists(template_path):
-            raise Http404(f"Template não encontrado: {template_path}")
-        return super().get(request, *args, **kwargs)
+#     def get(self, request, *args, **kwargs):
+#         template_path = os.path.join(settings.TEMPLATES[0]['DIRS'][0], self.template_name)
+#         if not os.path.exists(template_path):
+#             raise Http404(f"Template não encontrado: {template_path}")
+#         return super().get(request, *args, **kwargs)
