e83458c9b70b73c29caed98e98319b83a20fcd87
GuilhermeK <guilhermekscampos@gmail.com>
2025-01-06 21:55:38 -0300

    att

--- backend/settings/production.py
+++ backend/settings/production.py
@@ -6,9 +6,9 @@ from corsheaders.defaults import default_headers
 
 DEBUG = False
 
-# BASE_URL = "https://licitacao360.com"
+BASE_URL = "https://licitacao360.com"
 
-# ALLOWED_HOSTS = ['licitacao360.com', 'www.licitacao360.com']
+ALLOWED_HOSTS = ['licitacao360.com', 'www.licitacao360.com']
 
 # Segurança em produção
 SECURE_BROWSER_XSS_FILTER = True
@@ -17,10 +17,10 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SECURE = True
 X_FRAME_OPTIONS = 'DENY'
 
-# CORS_ALLOWED_ORIGINS = [
-#     "https://www.licitacao360.com",
-#     "https://licitacao360.com",
-# ]
+CORS_ALLOWED_ORIGINS = [
+    "https://www.licitacao360.com",
+    "https://licitacao360.com",
+]
 
 # CORS_ALLOW_HEADERS = list(default_headers) + [
 #     'X-CSRFToken',
